89630218c6c2588f46a9f669162af11e3b0a6db4
jan_kiezun <j4n3k777@gmail.com>
2024-09-16 20:37:03 +0200

    test bbc url scrapper

---
+++ main.py
@@ -0,0 +1,22 @@
+﻿from scrapper_src.news_sources.bbc import BBC_scrapper
+
+save_dir = "data"
+article_urls_file = "bbc_article_urls.jsonl"
+articles_file = "bbc_articles.jsonl"
+
+
+def main():
+    bbc_scrapper = BBC_scrapper(
+        save_dir=f"{save_dir}/bbc",
+        save_file=articles_file,
+        save_urls_file=article_urls_file,
+        verbose=True,
+    )
+    bbc_scrapper.scrape_article_urls(from_file=False)
+    bbc_scrapper.save_article_urls_to_json(
+        f"{save_dir}/bbc/{article_urls_file}", pretty=True
+    )
+
+
+if __name__ == "__main__":
+    main()
